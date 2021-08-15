f2c7a9be4504afbfc5f650dac55d9e51898f53c9
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-08-15 20:55:42 +0100

    Initial test of maplibre-compare using vite and vue3

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+body {
+  overflow: hidden;
+  margin: 0;
+}
+
+body * {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="comparison-container">
+    <div id="before" class="map"></div>
+    <div id="after" class="map"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  },
+  mounted() {
+    maplibregl.accessToken = 'pk.eyJ1IjoibGVpZmdlaHJtYW5uIiwiYSI6Ik4waTNoeGMifQ.320CRn54CJk41-Dbm4iSLQ';
+    var beforeMap = new maplibregl.Map({
+      container: "before",
+      style: 'mapbox://styles/leifgehrmann/cksdm6ebv3u8417p1amevc6d6',
+      center: [7.221275, 50.326111],
+      zoom: 5,
+    });
+
+    var afterMap = new maplibregl.Map({
+      container: "after",
+      style:
+          "mapbox://styles/leifgehrmann/ckqqx35za2evv18o0vzlrmib1",
+      center: [7.221275, 50.326111],
+      zoom: 5,
+    });
+
+    // A selector or reference to HTML element
+    var container = "#comparison-container";
+
+    // @ts-ignore
+    var map = new maplibregl.Compare(beforeMap, afterMap, container);
+  }
+})
+</script>
+
+<style scoped>
+.map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  background: #0F0;
+}
+</style>
