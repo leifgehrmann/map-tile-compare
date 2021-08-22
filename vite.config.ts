1f1a6888b5fa0591ed44cb7f52f413dc252061ae
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-08-22 17:17:29 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import istanbul from 'vite-plugin-istanbul';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
