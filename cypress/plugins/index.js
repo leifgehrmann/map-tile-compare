1ff5aee837230705c7bab418a69e41acd50685e1
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-08-22 16:18:41 +0100

    Managed to get code coverage with cypress!

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 
-module.exports = on => {
+module.exports = (on, config) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('@cypress/code-coverage/task')(on, config)
   on('file:preprocessor', cypressTypeScriptPreprocessor)
+  return config
 }
