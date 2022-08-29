406442c0709fc49784bfd46aad021ca5d08518bc
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-08-29 20:10:27 +0100

    NPM Update (devDependencies) + Linting fixes

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,7 +2,7 @@
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
 module.exports = (on, config) => {
-  // eslint-disable-next-line max-len
+  // eslint-disable-next-line vue/max-len
   // eslint-disable-next-line global-require,import/no-extraneous-dependencies,@typescript-eslint/no-var-requires
   require('@cypress/code-coverage/task')(on, config);
   on('file:preprocessor', cypressTypeScriptPreprocessor);
